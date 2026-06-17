0abb3126548db46d6f5ff62c6aa282ff9822c305
Raj Kamal <rajkamaldhan@gmail.com>
2026-06-18 01:32:52 +0530

    feat: render backend url as baseURL

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://moodify-ai-backend-mal5.onrender.com",
+    baseURL: "https://moodify-ai-backendv3.onrender.com",
     withCredentials: true
 })
 
