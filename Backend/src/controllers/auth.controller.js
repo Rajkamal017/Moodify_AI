98ea11b95693f7b7a63627c934fb048f7410f28d
Raj Kamal <rajkamaldhan@gmail.com>
2026-06-17 22:47:10 +0530

    feat: implement user authentication controller with registration, login, profile retrieval, and logout logic

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -101,7 +101,7 @@ async function logoutUser(req, res){
 
     res.clearCookie("token")
 
-    await redis.set("token", Date.now().toString(), "EX", 60*60)
+    await redis.set(token, Date.now().toString(), "EX", 60*60)
 
     res.status(200).json({
         message: " Logout Successfully "
